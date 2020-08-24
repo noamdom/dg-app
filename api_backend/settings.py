a3aca1a627579a2e60b20873b849e58183fd5ac6
Noam Domovich <ndomovich@gmail.com>
2020-08-24 13:49:05 +0300

    heroku-django locals()

--- api_backend/settings.py
+++ api_backend/settings.py
@@ -11,8 +11,10 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
@@ -124,3 +126,8 @@ USE_TZ = True
 # take care about make static files (css,js and etc')
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+
+
+
+# take care about using static files
+django_heroku.settings(locals())
